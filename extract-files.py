LineageOS/android_device_sony_sm8250-common

sm8250-common: Patch libsomc_{camerahal,chokoballcmn} with libui-v34
* Great google changes for android 16, caused our camerahal to crash
  if anything related to motion tracking / Auto Focus was enabled in
  Sony PRO apps.

Change-Id: I604dc21808434a00c43b35a030e27f10836fb021

extract-files.py
@@ -106,6 +106,11 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/lib64/libwvhidl.so',
     ): blob_fixup()
         .add_needed('libcrypto_shim.so'),
+    (
+        'vendor/lib64/libsomc_camerahal.so',
+        'vendor/lib64/libsomc_chokoballcmn.so',
+    ): blob_fixup()
+        .replace_needed('libui.so', 'libui-v34.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(

1036664317/android_device_sony_sm8150-common

extract-files.py
@@ -102,6 +102,11 @@ blob_fixups: blob_fixups_user_type = {
             .add_needed('libgui_shim.so'),
         'system_ext/bin/wfdservice': blob_fixup()
             .add_needed('libwfdservice_shim.so'),
+        (
+        'vendor/lib/libsomc_camerahal.so',
+        'vendor/lib/libsomc_chokoballcmn.so',
+        ): blob_fixup()
+        .replace_needed('libui.so', 'libui-v34.so'),
 }  # fmt: skip
 
 lib_fixups: lib_fixups_user_type = {