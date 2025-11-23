4b246234faa677e85f9a946a4a636f35ecefc0c1
voidanix <voidanix@keyedlimepie.org>
2025-11-23 19:27:04 +0100

    sm8150-common: build QTI AIDL thermal HAL
    Signed-off-by: voidanix <voidanix@keyedlimepie.org>

--- common.mk
+++ common.mk
@@ -396,6 +396,10 @@ PRODUCT_PACKAGES += \
     update_engine_sideload \
     update_verifier
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal-service.qti
+
 # Touch
 PRODUCT_PACKAGES += \
     vendor.lineage.touch-service.sony
